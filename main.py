286ce4a91eadfb6bceb74fd1fdeb0b73076d6df1
Tristan <86117774+knockout101@users.noreply.github.com>
2023-10-21 14:25:53 -0500

    working prototype for thermocouple temperature

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ import adafruit_max31856
 
 spi = board.SPI()
 
+cs = digitalio.DigitalInOut(board.D5)
+cs.direction = digitalio.Direction.OUTPUT
 
+thermocouple = adafruit_max31856.MAX31856(spi, cs)
+
+print(thermocouple.temperature)
