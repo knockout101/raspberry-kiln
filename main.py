aa270d2eb88acc3b5c107baf24b91bb8d00c7085
unknown <86117774+ThisIsMyFirstTime@users.noreply.github.com>
2023-10-22 17:18:39 -0500

    added RPi.GPIO code for setup of channel 31 to output 3V

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import board
 import digitalio
 import adafruit_max31856
+import RPi.GPIO as gpio
+
+# Setting the mode for which the channels will be called
+gpio.setmode(gpio.BOARD)
+print(f'\n[GPIO SETMODE: {gpio.getmode().upper()}]\n')
+
+# Setting up the actual channel to output
+gpio.setup(31, gpio.OUT)
+print(f"\n[GPIO SETUP CHANNEL 31]\n")
+
+# Setting output state to high = 3V
+gpio.output(31, gpio.HIGH)
+print(f"\n[GPIO OUTPUT STATE HIGH SET TO CHANNEL 31]\n")
 
 spi = board.SPI()
 
