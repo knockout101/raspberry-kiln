a6d2e81a73aac08642c047efc30372350a4116d2
unknown <86117774+ThisIsMyFirstTime@users.noreply.github.com>
2023-10-29 23:05:04 -0500

    changed direction back to OUTPUT and set .output to 0

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ import RPi.GPIO as gpio
 print(f'\n[GPIO SETMODE: {gpio.getmode()}]\n')
 
 # Setting up the actual channel to output
-gpio.setup(31, gpio.OUT)
+gpio.setup(31, gpio.IN)
 print(f"\n[GPIO SETUP CHANNEL 31]\n")
 
 # Setting output state to high = 3V
-gpio.output(31, gpio.HIGH)
+gpio.output(31, 0)
 print(f"\n[GPIO OUTPUT STATE HIGH SET TO CHANNEL 31]\n")
 
 spi = board.SPI()
 
 cs = digitalio.DigitalInOut(board.D5)
-cs.direction = digitalio.Direction.INPUT
+cs.direction = digitalio.Direction.OUTPUT
 
 thermocouple = adafruit_max31856.MAX31856(spi, cs)
 
