8dbf6baa7820a5b7a7b5f3cb7210dbaf6ecb3e27
unknown <86117774+ThisIsMyFirstTime@users.noreply.github.com>
2023-10-29 23:44:42 -0500

    while loop setup using digitalio to switch between in and out directions for GPIO pin D6

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ import board
 import digitalio
 import adafruit_max31856
 import RPi.GPIO as gpio
+from time import sleep
 
 # Showing current GPIO mode
 print(f'\n[GPIO SETMODE: {gpio.getmode()}]\n')
 
 # Setting up the actual channel to output
-cs1 = digitalio.DigitalInOut(board.D6)
-cs1.direction = digitalio.Direction.INPUT
+i = 1
+pin_D6 = digitalio.DigitalInOut(board.D6)
+while (i < 10):
+    if (i % 2 == 0):
+        pin_D6.direction = digitalio.Direction.INPUT
+    else:
+        pin_D6.direction = digitalio.Direction.OUTPUT
+    i += 1
+    sleep(0.2)
 print("\n[DIGITALIO PIN D6 SET TO INPUT]\n")
 
 # Setting output state to high = 3V
