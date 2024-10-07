3b2225ec08d92f5c1cf7c5ae8185c4da64b22b88
Tristan <86117774+knockout101@users.noreply.github.com>
2024-10-07 18:22:46 -0500

    adding pipfile for matplotlib and the new program schedule_parser.py containing the simple plotting program that comes with the kiln program main.py, renamed main.py to kiln.py

---
+++ schedule_parser.py
@@ -0,0 +1,23 @@
+import csv
+import sys
+from matplotlib import pyplot as plt
+
+temp_data = []
+time_data = []
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Add data file as argument to program call")
+        exit(1)
+
+    with open(sys.argv[1], "w+") as file:
+        rows = csv.reader(file)
+        for row in rows:
+            temp_data.append(float(row[0]))
+            time_data.append(float(row[1]))
+
+    ax = plt.gca()
+    ax.set_ylim([0, 1300])
+    ax.set_yticks(range(0, 1300, 100))
+    plt.plot(time_data, temp_data)
+    plt.show()
